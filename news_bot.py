d236b9d8f6bf3eaf2c8fe756592b2ed8ca55bdba
Isaiah Akinlabi <Isaiahakinlabi99@gmail.com>
2026-03-18 21:56:15 +0100

    news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -79,12 +79,12 @@ html_content = f"""
 msg = MIMEText(html_content, "html")
 msg["Subject"] = "Daily News Summary"
 msg["From"] = EMAIL
-msg["To"] = ", ".join(recipients)
+msg["To"] = ",".join(RECIPIENTS)
 
 # Use SMTP_SSL for port 465 to establish an SSL-wrapped connection
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(EMAIL, PASSWORD)
-    server.sendmail(EMAIL, recipients, msg.as_string())
+    server.sendmail(EMAIL, RECIPIENTS, msg.as_string())
 
 print("Email sent")
 import os
@@ -92,4 +92,4 @@ import os
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 EMAIL = os.getenv("EMAIL_SENDER")
 PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
-recipients = os.getenv("RECIPIENTS").split(",")
+RECIPIENTS = os.getenv("RECIPIENTS").split(",")
